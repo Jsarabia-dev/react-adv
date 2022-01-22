d6062fbf12fc69ac463ded90a9e30eb976201fa1
jsarabiav <jsarabiav@ripley.com>
2022-01-22 20:52:07 -0300

    add normal component

--- src/02-components-patterns/pages/ShoppingPage.tsx
+++ src/02-components-patterns/pages/ShoppingPage.tsx
@@ -1,9 +1,27 @@
-import React from 'react';
+import ProductCard from '../components/ProductCard';
+
+const product = {
+  id: '1',
+  img: './coffee-mug.png',
+  title: 'title1',
+};
 
 const ShoppingPage = () => {
   return (
     <div>
       <h1> ShoppingPage </h1>
+      <hr />
+      <div style={{
+        display: 'flex',
+        flexDirection: 'row',
+        flexWrap: 'wrap'
+      }}>
+        <ProductCard product={ product }/>
+        <ProductCard product={ product }/>
+        <ProductCard product={ product }/>
+        <ProductCard product={ product }/>
+        <ProductCard product={ product }/>
+      </div>
     </div>
   );
 };
